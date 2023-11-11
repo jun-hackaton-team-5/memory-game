ce93c65d39c8cd477c9c7229ac3950673eef160c
Nikolay Gagarinov <feycot@gmail.com>
2023-11-11 13:17:56 +0500

    add boilerplate bootstrap

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Http\File;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,5 +15,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('index.html');
+    $indexContent = file_get_contents(resource_path('views/index.html'));
+
+    return $indexContent;
 });
