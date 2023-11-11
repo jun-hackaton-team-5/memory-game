fbf25069152a35f966f06e07201aeb1b3281ebaf
Nikolay Gagarinov <feycot@gmail.com>
2023-11-11 22:05:29 +0500

    add flow

--- public/game.js
+++ public/game.js
@@ -1,52 +1,90 @@
 const CARDS_STATES = {
-    open: 'open',
+    finished: 'finished',
     closed: 'closed',
     comparing: 'comparing',
+    invalid: 'invalid'
 };
 
-const buildOpenCardUrl = (color) => `https://placehold.co/50x50/${color}/white?text=${color}`;
-const buildClosedCardUrl = (color) => `https://placehold.co/50x50/gray/white?text=Closed`;
+const buildOpenCardUrl = (id) => `https://placehold.co/50x50/gray/white?text=${id}`;
+const buildClosedCardUrl = () => `https://placehold.co/50x50/white/black?text=Closed`;
 
-const buildCardElement = (cardData) => {
+const buildCardElement = ({ id, name, state }) => {
     const el = document.createElement('img');
-    const url = cardData.state === CARDS_STATES.open
-        ? buildOpenCardUrl(cardData.name)
-        : buildClosedCardUrl(cardData);
+
+    let url = '';
+    if (state === CARDS_STATES.finished) {
+        url = `https://placehold.co/50x50/green/white?text=${name}`;
+    } else if (state === CARDS_STATES.comparing) {
+        url = `https://placehold.co/50x50/gray/white?text=${name}`;
+    } else if (state === CARDS_STATES.closed) {
+        url = `https://placehold.co/50x50/white/black?text=Closed`;
+    } else if (state === CARDS_STATES.invalid) {
+        url = `https://placehold.co/50x50/red/white?text=${name}`;
+    } else {
+        throw Error("invalid card state");
+    }
 
     el.setAttribute('src', url);
 
-    el.dataset.name = cardData.name;
-    el.dataset.state = cardData.state;
-    el.dataset.id = cardData.id;
+    el.dataset.name = name;
+    el.dataset.state = state;
+    el.dataset.id = id;
 
     return el;
 }
 
-
-
-const changeState = (cardElement) => {
-    const { name, state } = cardElement.dataset;
-
-    const nextState =
-        state === CARDS_STATES.open ? CARDS_STATES.closed : CARDS_STATES.open;
-
-    const nextElement = buildCardElement({name, state: nextState});
-    cardElement.replaceWith(nextElement);
-};
-
 const shuffle = (elements) => elements
     .map(value => ({ value, sort: Math.random() }))
-    .sort((a, b) => a.sort - b.sort)
+    // .sort((a, b) => a.sort - b.sort)
     .map(({ value }) => value);
 
 /**
  * @param {HTMLElement} gameField
  * @param {HTMLElement} cardEl
  */
-const handleClickCard = (gameField, cardEl) => {
-    const { id, name, state } = cardEl.dataset;
-    changeState(cardEl);
-    console.log({id, name, state});
+const handleClickCard = (gameField, clickedCard) => {
+    const { dataset: clickedCardData } = clickedCard;
+    const anotherCard = gameField
+        .querySelector(`[data-state="${CARDS_STATES.comparing}"]:not([data-id="${clickedCardData.id}"])`);
+
+    if (clickedCardData.state !== CARDS_STATES.closed) {
+        return;
+    }
+
+    // NOTE: открытую карту помечаем для сравнения
+    clickedCardData.state = CARDS_STATES.comparing;
+    clickedCard.replaceWith(buildCardElement({ ...clickedCardData}));
+    const newClickedCard = document.querySelector(`[data-id="${clickedCardData.id}"]`);
+
+    if (anotherCard === null) {
+        return;
+    }
+
+    if (anotherCard.dataset.name === clickedCard.dataset.name) {
+        newClickedCard.replaceWith(
+            buildCardElement({ ...clickedCardData, state: CARDS_STATES.finished })
+        );
+        anotherCard.replaceWith(
+            buildCardElement({ ...anotherCard.dataset, state: CARDS_STATES.finished })
+        );
+    } else {
+        newClickedCard.replaceWith(
+            buildCardElement({ ...clickedCardData, state: CARDS_STATES.invalid })
+        );
+        anotherCard.replaceWith(
+            buildCardElement({ ...anotherCard.dataset, state: CARDS_STATES.invalid })
+        );
+
+        setTimeout(() => {
+            gameField
+                .querySelectorAll(`[data-state="${CARDS_STATES.invalid}"]`)
+                .forEach((cardElement) => {
+                    cardElement.replaceWith(
+                        buildCardElement({ ...cardElement.dataset, state: CARDS_STATES.closed })
+                    );
+            });
+        }, 1000);
+    }
 };
 
 const prepareCards = () => {
